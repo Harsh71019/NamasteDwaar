e6bb7b346c6c715e19c8ef1fe64d377e0a15e423
harsh71019 <harsh710patel@gmail.com>
2022-02-05 00:46:01 +0530

    Add more pages

--- components/experiences/Section2.js
+++ components/experiences/Section2.js
@@ -21,7 +21,7 @@ const Section2 = () => {
                     <p className='experiences-types__card-desc'>
                       {exp.mainDescription}
                     </p>
-                    <Link href={`/experience/${exp.id}`}>
+                    <Link href={`/experiences/${exp.id}`}>
                       <a className='experiences-types__card-link'>Explore</a>
                     </Link>
                   </div>
