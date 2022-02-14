fbc88306e4cecbb3e03f76eca2a80960bb189721
harsh71019 <harsh710patel@gmail.com>
2022-02-14 16:46:45 +0530

    making cards clickable

--- components/experiences/Section2.js
+++ components/experiences/Section2.js
@@ -9,40 +9,40 @@ const Section2 = () => {
         <div className='container'>
           <div className='row experiences-types__card'>
             {experiencesDetails.map((exp) => (
-              <div
-                key={exp.id}
-                className='col-12 col-md-6 experiences-types__col'
-              >
-                <div className='experiences-types__card-body '>
-                  <div className='px-3 sss'>
-                    <h1 className='experiences-types__card-heading'>
-                      {exp.mainTitle}
-                    </h1>
-                    <p className='experiences-types__card-desc'>
-                      {exp.mainDescription}
-                    </p>
-                    <Link href={`/experiences/${exp.id}`}>
+              <Link href={`/experiences/${exp.id}`}>
+                <div
+                  key={exp.id}
+                  className='col-12 col-md-6 experiences-types__col'
+                >
+                  <div className='experiences-types__card-body '>
+                    <div className='px-3 sss'>
+                      <h1 className='experiences-types__card-heading'>
+                        {exp.mainTitle}
+                      </h1>
+                      <p className='experiences-types__card-desc'>
+                        {exp.mainDescription}
+                      </p>
+                      <Link href={`/experiences/${exp.id}`}>
+                        <a className='experiences-types__card-link'>Explore</a>
+                      </Link>
+                    </div>
+                    <img
+                      src={exp.image}
+                      alt={exp.id}
+                      className='w-100 experiences-types__card-image'
+                    />
+                    <div className='px-3 hbs'>
+                      <h1 className='experiences-types__card-heading'>
+                        {exp.mainTitle}
+                      </h1>
+                      <p className='experiences-types__card-desc'>
+                        {exp.mainDescription}
+                      </p>
                       <a className='experiences-types__card-link'>Explore</a>
-                    </Link>
-                  </div>
-                  <img
-                    src={exp.image}
-                    alt={exp.id}
-                    className='w-100 experiences-types__card-image'
-                  />
-                  <div className='px-3 hbs'>
-                    <h1 className='experiences-types__card-heading'>
-                      {exp.mainTitle}
-                    </h1>
-                    <p className='experiences-types__card-desc'>
-                      {exp.mainDescription}
-                    </p>
-                    <Link href={`/experiences/${exp.id}`}>
-                      <a className='experiences-types__card-link'>Explore</a>
-                    </Link>
+                    </div>
                   </div>
                 </div>
-              </div>
+              </Link>
             ))}
           </div>
         </div>
